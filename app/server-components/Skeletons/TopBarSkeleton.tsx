dee9905070af3b3d4f3e2e4a45e90acc2102472f
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-03-05 15:23:09 +0200

    TopBarSkeleton

--- app/server-components/Skeletons/TopBarSkeleton.tsx
+++ app/server-components/Skeletons/TopBarSkeleton.tsx
@@ -2,13 +2,13 @@ import { Skeleton } from '@heroui/skeleton';
 
 export default function TopBarSkeleton() {
     return (
-        <div className="row w-full justify-between gap-12">
+        <div className="flex w-full flex-col justify-between gap-4 md:gap-6 lg:flex-row lg:gap-12">
             <div className="w-full flex-1">
-                <Skeleton className="h-[52px] rounded-2xl" />
+                <Skeleton className="h-[52px] w-full rounded-xl sm:w-auto" />
             </div>
 
             <div className="flex-1">
-                <div className="row justify-end gap-3">
+                <div className="hidden flex-wrap items-center justify-between gap-2 sm:flex lg:flex-nowrap lg:justify-end lg:gap-3">
                     {Array(3)
                         .fill(null)
                         .map((_, index) => (
@@ -16,6 +16,10 @@ export default function TopBarSkeleton() {
                                 <Skeleton className="h-[52px] min-w-40 rounded-full" />
                             </div>
                         ))}
+                </div>
+
+                <div className="flex w-full sm:hidden">
+                    <Skeleton className="h-[122px] w-full rounded-xl" />
                 </div>
             </div>
         </div>

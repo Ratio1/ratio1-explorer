f98612a0243b2a8984a4646fc63e0c2ddbf78379
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-03-10 15:38:51 +0200

    Fix hot refresh on development

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = withBundleAnalyzer({
     enabled: false,
-})({});
+})({
+    generateBuildId: async () => {
+        return 'ratio1-explorer';
+    },
+    webpack: (config, { dev }) => {
+        if (!dev) {
+            // Only apply these settings in production
+            config.output.filename = config.output.filename.replace('[chunkhash]', '[contenthash]');
+            config.optimization.moduleIds = 'deterministic';
+            config.optimization.chunkIds = 'deterministic';
+        }
+        return config;
+    },
+    output: 'standalone',
+});
 
 export default nextConfig;

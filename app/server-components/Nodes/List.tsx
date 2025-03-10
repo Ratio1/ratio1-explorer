493c15829dbc553521fa60001f87dae76edf3117
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-03-10 16:07:37 +0200

    Implement ListHeader for the nodes list

--- app/server-components/Nodes/List.tsx
+++ app/server-components/Nodes/List.tsx
@@ -3,6 +3,7 @@ import * as types from '@/typedefs/blockchain';
 import { R1Address } from '@/typedefs/blockchain';
 import { Skeleton } from '@heroui/skeleton';
 import { Suspense } from 'react';
+import ListHeader from '../shared/ListHeader';
 import Node from './Node';
 
 export default async function List({
@@ -17,6 +18,15 @@ export default async function List({
     return (
         <div className="col flex-1 justify-between gap-8">
             <div className="group/list col w-full gap-2 overflow-x-auto">
+                <ListHeader>
+                    <div className="min-w-[200px] lg:min-w-[228px]">Alias</div>
+                    <div className="min-w-[164px]">Addresses</div>
+                    <div className="min-w-[244px]">License</div>
+                    <div className="min-w-[112px]">Owner</div>
+                    <div className="min-w-[50px]">Version</div>
+                    <div className="min-w-[152px]">Last Epoch Availability</div>
+                </ListHeader>
+
                 {Object.entries(nodes).map(([ratio1Addr, node]) => (
                     <div key={ratio1Addr}>
                         <Suspense fallback={<Skeleton className="min-h-[92px] w-full rounded-2xl" />}>

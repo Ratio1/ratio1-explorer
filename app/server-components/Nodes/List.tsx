a18b4d6487675d42ec59af2fff5c8132af88b888
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-06-23 19:59:54 +0300

    Fix visual bug when Node returns null

--- app/server-components/Nodes/List.tsx
+++ app/server-components/Nodes/List.tsx
@@ -2,7 +2,7 @@ import ParamsPagination from '@/components/Nodes/ParamsPagination';
 import * as types from '@/typedefs/blockchain';
 import { R1Address } from '@/typedefs/blockchain';
 import { Skeleton } from '@heroui/skeleton';
-import { Suspense } from 'react';
+import { Fragment, Suspense } from 'react';
 import ListHeader from '../shared/ListHeader';
 import Node from './Node';
 
@@ -28,11 +28,11 @@ export default async function List({
                 </ListHeader>
 
                 {Object.entries(nodes).map(([ratio1Addr, node]) => (
-                    <div key={ratio1Addr}>
+                    <Fragment key={ratio1Addr}>
                         <Suspense fallback={<Skeleton className="min-h-[92px] w-full rounded-2xl" />}>
                             <Node ratio1Addr={ratio1Addr as R1Address} node={node} />
                         </Suspense>
-                    </div>
+                    </Fragment>
                 ))}
             </div>
 

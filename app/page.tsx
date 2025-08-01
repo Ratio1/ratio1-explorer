13e23955f5c8a0d861873fa6b798bf6c2eab647e
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-08-01 10:42:21 +0300

    Better error handling for the HomePage API call

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { getActiveNodes } from '@/lib/api';
 import * as types from '@/typedefs/blockchain';
 import { Suspense } from 'react';
+import { RiErrorWarningLine } from 'react-icons/ri';
 import Hero from './server-components/Nodes/Hero';
 import List from './server-components/Nodes/List';
 import NodesListSkeleton from './server-components/Skeletons/NodesListSkeleton';
+import { Alert } from './server-components/shared/Alert';
 
 export default async function HomePage(props: {
     searchParams?: Promise<{
@@ -13,21 +15,36 @@ export default async function HomePage(props: {
     const searchParams = await props.searchParams;
     const currentPage = Number(searchParams?.page) || 1;
 
-    const response: types.OraclesDefaultResult = await getActiveNodes(currentPage);
+    try {
+        const response: types.OraclesDefaultResult = await getActiveNodes(currentPage);
 
-    const nodes: {
-        [key: string]: types.NodeState;
-    } = response.result.nodes;
+        const nodes: {
+            [key: string]: types.NodeState;
+        } = response.result.nodes;
 
-    const pagesCount: number = response.result.nodes_total_pages;
+        const pagesCount: number = response.result.nodes_total_pages;
 
-    return (
-        <>
-            <Hero nodesTotalItems={response.result.nodes_total_items} resourcesTotal={response.result.resources_total} />
+        return (
+            <>
+                <Hero nodesTotalItems={response.result.nodes_total_items} resourcesTotal={response.result.resources_total} />
 
-            <Suspense fallback={<NodesListSkeleton />}>
-                <List nodes={nodes} pagesCount={pagesCount} currentPage={currentPage} />
-            </Suspense>
-        </>
-    );
+                <Suspense fallback={<NodesListSkeleton />}>
+                    <List nodes={nodes} pagesCount={pagesCount} currentPage={currentPage} />
+                </Suspense>
+            </>
+        );
+    } catch (error) {
+        console.log('Failed to fetch active nodes:', error);
+
+        return (
+            <div className="mx-auto w-full flex-1 px-4 py-8">
+                <Alert
+                    variant="warning"
+                    icon={<RiErrorWarningLine className="text-lg" />}
+                    title="Failed to fetch nodes"
+                    description="An error occured while trying to fetch the active nodes. If the problem persists, please contact support."
+                />
+            </div>
+        );
+    }
 }

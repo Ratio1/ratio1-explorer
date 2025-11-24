1f70b153f67a5613a22545371ab8c850db7a495d
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-11-24 15:50:49 +0200

    RPCs load balancing

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -3,18 +3,28 @@
 import config, { chain } from '@/config';
 import { createPublicClient, http, PublicClient } from 'viem';
 
-let publicClient: PublicClient | null = null;
+if (!process.env.NEXT_PUBLIC_RPC) {
+    throw new Error('NEXT_PUBLIC_RPC is not set');
+}
 
-export const getPublicClient = async () => {
-    if (!publicClient) {
-        // ! Replace with a paid RPC at some point
-        publicClient = createPublicClient({
-            chain,
-            transport: http(
-                `https://base-${config.environment === 'mainnet' ? 'mainnet' : 'sepolia'}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-            ),
-        }) as PublicClient;
-    }
+if (!process.env.ALCHEMY_API_KEY) {
+    throw new Error('ALCHEMY_API_KEY is not set');
+}
 
-    return publicClient;
+const publicNodeRPC: string = process.env.NEXT_PUBLIC_RPC;
+const alchemyRPC: string = `https://base-${config.environment === 'mainnet' ? 'mainnet' : 'sepolia'}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
+
+const publicNodeClient: PublicClient = createPublicClient({
+    chain,
+    transport: http(publicNodeRPC),
+}) as PublicClient;
+
+const alchemyClient: PublicClient = createPublicClient({
+    chain,
+    transport: http(alchemyRPC),
+}) as PublicClient;
+
+export const getPublicClient = async (): Promise<PublicClient> => {
+    // 50/50 random selection
+    return Math.random() < 0.5 ? publicNodeClient : alchemyClient;
 };

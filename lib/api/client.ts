751897f7ce861ae8005238814d7ee53aa26b74fe
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-11-26 18:55:19 +0200

    Squashed commit of the following:
    commit a386495e50ed89f95ace79c7c81b95d78f0eb739
    Author: wzrdx <128477299+wzrdx@users.noreply.github.com>
    Date:   Wed Nov 26 18:55:02 2025 +0200
    
        Set PublicNode RPC and leave Alchemy only for dedicated calls
    
    commit 1f70b153f67a5613a22545371ab8c850db7a495d
    Author: wzrdx <128477299+wzrdx@users.noreply.github.com>
    Date:   Mon Nov 24 15:50:49 2025 +0200
    
        RPCs load balancing

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,20 +1,23 @@
 'use server';
 
-import config, { chain } from '@/config';
+import { chain } from '@/config';
 import { createPublicClient, http, PublicClient } from 'viem';
 
-let publicClient: PublicClient | null = null;
+if (!process.env.NEXT_PUBLIC_RPC) {
+    throw new Error('NEXT_PUBLIC_RPC is not set');
+}
 
-export const getPublicClient = async () => {
-    if (!publicClient) {
-        // ! Replace with a paid RPC at some point
-        publicClient = createPublicClient({
-            chain,
-            transport: http(
-                `https://base-${config.environment === 'mainnet' ? 'mainnet' : 'sepolia'}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-            ),
-        }) as PublicClient;
-    }
+if (!process.env.ALCHEMY_API_KEY) {
+    throw new Error('ALCHEMY_API_KEY is not set');
+}
 
-    return publicClient;
+const publicNodeRPC: string = process.env.NEXT_PUBLIC_RPC;
+
+const publicNodeClient: PublicClient = createPublicClient({
+    chain,
+    transport: http(publicNodeRPC),
+}) as PublicClient;
+
+export const getPublicClient = async (): Promise<PublicClient> => {
+    return publicNodeClient;
 };

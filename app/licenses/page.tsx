e96495a91236028fc3fd3adfb4765931de2f3c0e
Alessandro <defranceschi_a@yahoo.com>
2026-02-10 17:34:48 +0100

    refactor: update licenses page loading

--- app/licenses/page.tsx
+++ app/licenses/page.tsx
@@ -1,6 +1,7 @@
 import ErrorComponent from '@/app/server-components/shared/ErrorComponent';
-import { getAllLicenseTokenIds } from '@/lib/api/blockchain';
-import { LicenseItem } from '@/typedefs/general';
+import { PAGE_SIZE } from '@/config';
+import { getLicensesPage } from '@/lib/api/blockchain';
+import { LicenseListItem } from '@/typedefs/general';
 import { unstable_cache } from 'next/cache';
 import List from '../server-components/Licenses/List';
 import { BorderedCard } from '../server-components/shared/cards/BorderedCard';
@@ -22,7 +23,16 @@ export async function generateMetadata({ searchParams }: { searchParams?: Promis
     };
 }
 
-const getCachedLicenseTokenIds = unstable_cache(getAllLicenseTokenIds, ['licenses-token-ids'], { revalidate: 300 });
+const getCachedLicensesPage = unstable_cache(
+    async (currentPage: number) => {
+        const page = currentPage > 0 ? currentPage : 1;
+        const offset = (page - 1) * PAGE_SIZE;
+
+        return getLicensesPage(offset, PAGE_SIZE);
+    },
+    ['licenses-page'],
+    { revalidate: 300 },
+);
 
 export default async function LicensesPage(props: {
     searchParams?: Promise<{
@@ -32,24 +42,21 @@ export default async function LicensesPage(props: {
     const searchParams = await props.searchParams;
     const currentPage = Number(searchParams?.page) || 1;
 
-    let ndTotalSupply: number, mndTotalSupply: number;
-    let licenses: LicenseItem[];
+    let ndTotalSupply: bigint, mndTotalSupply: bigint;
+    let licensesCount: number;
+    let licenses: LicenseListItem[];
 
     try {
-        const { ndLicenseIds, mndLicenseIds } = await getCachedLicenseTokenIds();
-        ndTotalSupply = ndLicenseIds.length;
-        mndTotalSupply = mndLicenseIds.length;
+        const {
+            ndTotalSupply: ndSupply,
+            mndTotalSupply: mndSupply,
+            licenses: pageLicenses,
+        } = await getCachedLicensesPage(currentPage);
 
-        licenses = [
-            ...mndLicenseIds.map((licenseId) => ({
-                licenseId,
-                licenseType: licenseId === 1 ? ('GND' as const) : ('MND' as const),
-            })),
-            ...ndLicenseIds.map((licenseId) => ({
-                licenseId,
-                licenseType: 'ND' as const,
-            })),
-        ];
+        ndTotalSupply = ndSupply;
+        mndTotalSupply = mndSupply;
+        licenses = pageLicenses;
+        licensesCount = Number(ndTotalSupply + mndTotalSupply);
     } catch (error) {
         console.error(error);
         console.log('[Licenses Page] Failed to fetch license data');
@@ -65,17 +72,17 @@ export default async function LicensesPage(props: {
                     <div className="flexible-row">
                         <CardHorizontal
                             label="Total"
-                            value={ndTotalSupply + mndTotalSupply}
+                            value={licensesCount.toString()}
                             isFlexible
                             widthClasses="min-w-[192px]"
                         />
-                        <CardHorizontal label="ND" value={ndTotalSupply} isFlexible widthClasses="min-w-[192px]" />
-                        <CardHorizontal label="MND" value={mndTotalSupply} isFlexible widthClasses="min-w-[192px]" />
+                        <CardHorizontal label="ND" value={ndTotalSupply.toString()} isFlexible widthClasses="min-w-[192px]" />
+                        <CardHorizontal label="MND" value={mndTotalSupply.toString()} isFlexible widthClasses="min-w-[192px]" />
                     </div>
                 </BorderedCard>
             </div>
 
-            <List licenses={licenses} currentPage={currentPage} />
+            <List licenses={licenses} currentPage={currentPage} totalLicenses={licensesCount} />
         </>
     );
 }
